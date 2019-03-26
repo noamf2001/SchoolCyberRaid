34dac00b16498fc70d2568fcff04a632d252f29a
NOAMFLUSS <noamf2001@gmail.com>
2019-03-26 13:12:43 +0100

    add project נםםל

---
+++ RaidServer/AlgorithmMain.py
@@ -0,0 +1,9 @@
+def create_parity_file_part(file_path1,file_path2):
+    """
+    create the parity file out of there two.
+    :param file_path1:
+    :param file_path2:
+    :return path to the parity file
+            None if something wrong with those file
+    """
+    
