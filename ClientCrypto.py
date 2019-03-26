0a0558c80ff483ecc2818182a5d337e13fdb2405
NOAMFLUSS <noamf2001@gmail.com>
2019-03-26 12:56:54 +0100

    add server directory

--- ClientCrypto.py
+++ ClientCrypto.py
@@ -1,9 +1,20 @@
 import Crypto
 from Crypto.PublicKey import RSA
 from Crypto import Random
+import ast
 
 class ClientCrypto():
     def generate_key(self):
-        return
+        """
+        generate key for RSA
+        :return: the key - RSA type
+        """
+        random_generator = Random.new().read
+        key = RSA.generate(1024, random_generator)  # generate pub and priv key
+        self.key = key
+        self.public_key = key.publickey()
+
+
     def __init__(self):
-        self
+        self.generate_key()
+
