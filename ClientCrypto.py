d0bb6417d8759a85956ae57cb8afcf869ea7f224
NOAMFLUSS <noamf2001@gmail.com>
2019-03-26 12:34:10 +0100

    change to python2

---
+++ ClientCrypto.py
@@ -0,0 +1,9 @@
+import Crypto
+from Crypto.PublicKey import RSA
+from Crypto import Random
+
+class ClientCrypto():
+    def generate_key(self):
+        return
+    def __init__(self):
+        self
--- ClientMain.py
+++ ClientMain.py
@@ -9,3 +9,4 @@ class ClientMain():
     def __init__(self,SERVERIP,PORT):
         self.my_socket = socket.socket()
         self.my_socket.connect((SERVERIP, PORT))
+
