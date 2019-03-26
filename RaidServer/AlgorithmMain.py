80ec1bccaa30793a31923f478318568c20799950
NOAMFLUSS <noamf2001@gmail.com>
2019-03-26 15:28:03 +0100

    add Create_parity_file_part

--- RaidServer/AlgorithmMain.py
+++ RaidServer/AlgorithmMain.py
@@ -1,9 +1,12 @@
+import os
+
 """
 part file name:
 filename_partnumber_if it is a parity file - the other part number, else -1
 """
 
-PART_FILE_LENGTH = 100
+PART_FILE_LENGTH = 5
+
 
 def create_parity_file_part_path(file_path1, file_path2):
     start_of_filename = file_path1.rfind("\\") + 1
@@ -25,7 +28,7 @@ def create_parity_file_part(file_path1, file_path2):
             None if something wrong with those file
             save the file in file1 path
     """
-    parity_file_path = create_parity_file_part_path(file_path1,file_path2)
+    parity_file_path = create_parity_file_part_path(file_path1, file_path2)
 
     with open(file_path1, "rb") as file1:
         file_data1 = bytearray(file1.read())
@@ -37,12 +40,31 @@ def create_parity_file_part(file_path1, file_path2):
     with open(parity_file_path, "wb") as parity_file:
         parity_file.write(parity_file_data)
 
+
 def split_file(file_path):
-    with open(file_path,"rb") as file:
-
-        for i in range(len(file_path)/PART_FILE_LENGTH):
-            file_data = file_path.read()
-
+    """
+    split to file to part file with the same length as PART_FILE_LENGTH
+    complete the part file that(if exists) is not at that length with 0 (the ascii char with ascii value 0)
+    save the parts file as expected as above
+    :param file_path: the path of the file to split
+    :return: the number of file parts created
+    """
+    with open(file_path, "rb") as file:
+        parts_num = os.path.getsize(file_path) / PART_FILE_LENGTH
+        if len(file_path) % PART_FILE_LENGTH != 0:
+            parts_num += 1
+        for i in range(parts_num):
+            file_part_data = file.read(PART_FILE_LENGTH)
+            file_part_name = file_path[:file_path.rfind(".")] + "_" + str(i) + "_" + "-1" + file_path[
+                                                                                            file_path.rfind("."):]
+            with open(file_part_name, "wb") as file_part:
+                file_part.write(file_part_data)
+                print len(file_part_data)
+                if len(file_part_data) < PART_FILE_LENGTH:
+                    file_part.write(chr(0))
+        return parts_num
 
 if __name__ == '__main__':
-    print 1/2
+    split_file(r"C:\Users\Sharon\Documents\school\cyber\Project\try\somename.txt")
+    create_parity_file_part(r"C:\Users\Sharon\Documents\school\cyber\Project\try\somename_2_-1.txt",
+                            r"C:\Users\Sharon\Documents\school\cyber\Project\try\somename_1_-1.txt")
